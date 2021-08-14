ee48a0294c624ff451662a3afe4e1da21e4b3ec2
Arisara <86237314+Rinnum@users.noreply.github.com>
2021-08-14 23:36:42 +0700

    Add files via upload

---
+++ week1.1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int x;
+int y;
+int a;
+int b;
+int c;
+int d;
+printf("Enter number = ");
+scanf("%d", &x );
+scanf("%d" ,&y);
+a = x+y ,
+b = x-y ,
+c = x*y ,
+d = x/y ;
+printf("%d ",a);
+printf("%d ",b);
+printf("%d ",c);
+printf("%d ",d);
+return 0;
+}
